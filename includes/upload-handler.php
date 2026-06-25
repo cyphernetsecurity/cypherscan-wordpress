ffc1abb0892377cf7daf7af008a451c9ffde74ae
Gabriel Maheu <contact@cyphernetsecurity.com>
2026-06-25 14:30:18 -0400

    Add WordPress upload hook logging

--- includes/upload-handler.php
+++ includes/upload-handler.php
@@ -11,5 +11,13 @@ add_filter('wp_handle_upload', function ($upload) {
         error_log('[cypherscan-wordpress] file: ' . $upload['file']);
     }
 
+    if (isset($upload['url'])) {
+        error_log('[cypherscan-wordpress] url: ' . $upload['url']);
+    }
+
+    if (isset($upload['type'])) {
+        error_log('[cypherscan-wordpress] type: ' . $upload['type']);
+    }
+
     return $upload;
 });
